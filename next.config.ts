be8b1d19b14430b828238a2513e1802dad072d04
dhxaep <dhafagifari0412@gmail.com>
2026-06-01 15:55:39 +0700

    Remove pdf-parse from next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pdf-parse', '@sparticuz/chromium']
+  serverExternalPackages: ['@sparticuz/chromium']
 };
 
 export default nextConfig;
