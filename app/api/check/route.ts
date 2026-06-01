a1a940315e81372be35409b775c26dcbd6b918f7
dhxaep <dhafagifari0412@gmail.com>
2026-06-01 16:08:57 +0700

    Update next config for chromium-min

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -27,8 +27,10 @@ async function getBrowser() {
             }
         } else {
             // Serverless Netlify environment
-            const chromium = (await import('@sparticuz/chromium')).default;
-            executablePath = await chromium.executablePath();
+            const chromium = (await import('@sparticuz/chromium-min')).default;
+            executablePath = await chromium.executablePath(
+                "https://github.com/Sparticuz/chromium/releases/download/v131.0.1/chromium-v131.0.1-pack.tar"
+            );
             chromiumArgs = chromium.args;
         }
 
